7f19d44067b3d4fa9546b612e5a815ac69f90584
Oscar Ivan Garcia Perez <oigp96.10.05@gmail.com>
2023-03-24 23:47:04 -0600

    Add endpoints

---
+++ lib/model/end_point.dart
@@ -0,0 +1,7 @@
+class EndPoint {
+  final String baseUrlSearch = "https://pokeapi.co/api/v2/pokemon";
+  final String baseUrlImage =
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/";
+  final String baseURlImagePng =
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
+}
